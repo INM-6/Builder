7e2b46e5ba8814e83dadefd8d28e6f180026385c
Dennis Terhorst <d.terhorst@fz-juelich.de>
2021-03-10 20:44:50 +0100

    Revert "fix missing log directory"
    This reverts commit d669d7b0f5deee1d453dcbfcc87915657b32ca31.

--- build_functions.sh
+++ build_functions.sh
@@ -195,7 +195,6 @@ build_package () {
 	log_status ">>> build ${PACKAGE}/${VERSION}/${VARIANT}..."
 	cd "${BUILD}"
 	set -x
-	mkdir -pv "${LOG}"
 	"${SOURCE}/configure" --prefix="${TARGET}" --srcdir="${SOURCE}" ${CONFIGURE_OPTIONS:-} 2>&1 | tee "${LOG}/configure.log"
 	make -j ${MAKE_THREADS:-$(nproc)} 2>&1 | tee "${LOG}/make.log"
 	set +x
@@ -207,7 +206,6 @@ build_test () {
 
 build_install () {
 	log_status ">>> installing..."
-	mkdir -pv "${LOG}"
 	make install 2>&1 | tee "${LOG}/make-install.log"
 }
 
